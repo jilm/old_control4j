57441d2046b73844770849fee9c2a74767a58284
jlidinsky <jlidinsky@gmail.com>
2014-12-02 08:46:00 +0100

    Add a BoxFiller gui component.
    This component serves as a space delimeter for Box component.

---
+++ src/control4j/gui/components/BoxFiller.java
@@ -0,0 +1,117 @@
+package control4j.gui.components;
+
+/*
+ *  Copyright 2013, 2014 Jiri Lidinsky
+ *
+ *  This file is part of control4j.
+ *
+ *  control4j is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  control4j is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with control4j.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.awt.Dimension;
+import javax.swing.JComponent;
+
+import control4j.scanner.Getter;
+import control4j.scanner.Setter;
+
+/**
+ *
+ *  An empty rectangle (box) which may be used to place a space between
+ *  two components inside a Box component.
+ *
+ *  @see control4j.gui.components.Box
+ *
+ */
+@control4j.annotations.AGuiObject(name="Box filler")
+public class BoxFiller extends control4j.gui.VisualObject
+{
+
+  /** 
+   *  Size of the box. It depends on the axis of parent box if size
+   *  represents width or height.
+   */
+  private int size;
+
+  private boolean glue = false;
+
+  @Getter(key="Size")
+  public int getSize()
+  {
+    return size;
+  }
+
+  @Setter(key="Size")
+  public void setSize(int size)
+  {
+    this.size = size;
+    if (component != null)
+    {
+      ((javax.swing.Box.Filler)component).changeShape(getMinSize(), getPreffSize(), getMaxSize());
+    }
+  }
+
+  @Getter(key="Is Glue")
+  public boolean isGlue()
+  {
+    return glue;
+  }
+
+  @Setter(key="Is Glue")
+  public void setGlue(boolean isGlue)
+  {
+    glue = isGlue;
+    if (component != null)
+    {
+      ((javax.swing.Box.Filler)component).changeShape(getMinSize(), getPreffSize(), getMaxSize());
+    }
+  }
+
+  @Override
+  protected JComponent createSwingComponent()
+  {
+    return new javax.swing.Box.Filler(getMinSize(), getPreffSize(), getMaxSize());
+  }
+
+  @Override
+  protected void configureVisualComponent()
+  {
+  }
+
+  private Dimension getMinSize()
+  {
+    boolean isLineAxis = ((Box)getParent()).isLineAxis();
+    int size = glue ? 0 : this.size;
+    int width = isLineAxis ? size : 0;
+    int height = isLineAxis ? 0 : size;
+    return new Dimension(width, height);
+  }
+
+  private Dimension getMaxSize()
+  {
+    boolean isLineAxis = ((Box)getParent()).isLineAxis();
+    int size = glue ? Integer.MAX_VALUE : this.size;
+    int width = isLineAxis ? size : 0;
+    int height = isLineAxis ? 0 : size;
+    return new Dimension(width, height);
+  }
+
+  private Dimension getPreffSize()
+  {
+    boolean isLineAxis = ((Box)getParent()).isLineAxis();
+    int size = glue ? 0 : this.size;
+    int width = isLineAxis ? size : 0;
+    int height = isLineAxis ? 0 : size;
+    return new Dimension(width, height);
+  }
+
+}
