8225ae012f93353e12f24270ccdcd50e20316ed2
jlidinsky <jlidinsky@gmail.com>
2015-01-14 15:02:42 +0100

    Rewrite classes intended to export signals over tcp/ip.

--- src/control4j/protocols/tcp/Respondent.java
+++ src/control4j/protocols/tcp/Respondent.java
@@ -1,7 +1,7 @@
 package control4j.protocols.tcp;
 
 /*
- *  Copyright 2013, 2014 Jiri Lidinsky
+ *  Copyright 2013, 2014, 2015 Jiri Lidinsky
  *
  *  This file is part of control4j.
  *
@@ -19,19 +19,23 @@ package control4j.protocols.tcp;
  */
 
 import java.io.IOException;
+
 import control4j.tools.InterchangePoint;
 import static control4j.tools.LogMessages.*;
 import static control4j.tools.Logger.*;
 
 /**
  *
- *  Wait for request from a remote host and than send response. Typicaly
- *  it is used together with server to serve one connection.
+ *  Waits for new data request from a remote host and than send response. 
+ *  Typicaly it is used together with server to serve one connection.
  *
  *  <p>First of all you must use the read method to get a request from
  *  remote host. Then you can use the write method to respond to the
  *  request. The pair of opperations, send response, wait for request
  *  is done in a separate thred.
+ *
+ *  @see Server
+ *  @see IClientFactory
  *
  */
 public class Respondent<I, O> implements Runnable
@@ -42,13 +46,18 @@ public class Respondent<I, O> implements Runnable
   private InterchangePoint<I> request = new InterchangePoint<I>();
   private InterchangePoint<O> response = new InterchangePoint<O>();
 
-  /** Signal that the request has been received. */
-  private boolean gotRequest = false;
+  /** Signal that the new data request has been received. */
+  private volatile boolean gotRequest = false;
 
   /** Contains an exception thrown during proccessing, if there is one */
-  private Exception exception = null;
+  private volatile IOException exception = null;
 
   /**
+   *  Initialize
+   *
+   *  @param inputStream
+   *
+   *  @param outputStream
    */
   public Respondent(IInputStream<I> inputStream, IOutputStream<O> outputStream)
   {
@@ -57,7 +66,8 @@ public class Respondent<I, O> implements Runnable
   }
 
   /**
-   *  Close the connection and release all of the resources.
+   *  Close the given input and output stream and release all of the 
+   *  resources.
    */
   public void close()
   {
@@ -74,56 +84,110 @@ public class Respondent<I, O> implements Runnable
   }
 
   /**
+   *  Going to send response message. The response is send in separate
+   *  thread, so this method returns immediately and doesn't block.
+   *  The response is send only under condition that data request was
+   *  received since the last response was sent and that the data
+   *  request was picked up by the <code>read</code> method.
+   *
+   *  @param response
+   *             a message to send
+   *
+   *  @throws IOException
+   *             if an exception was thrown while waiting for new data
+   *             request or while sending a response
+   *
+   *  @throws IllegalStateException
+   *             if you call this method without a new data request or
+   *             without reading it
+   *
+   *  @see #read
    */
-  public void write(O response)
+  public void write(O response) throws IOException
   {
-    this.response.forcedSet(response);
-    this.request.forcedSet(null);
-    new Thread(this).start();
+    if (exception != null) throw exception;
+    if (gotRequest && request.isEmpty())
+    {
+      this.response.forcedSet(response);
+      new Thread(this).start();
+    }
+    else
+    {
+      throw new IllegalStateException(
+	  "Trying to send data response without a request");
+    }
   }
 
   /**
-   *  Return a request which has been received from a remote host. It returns
-   *  null if the response has not been already got. Once the request is
-   *  picked up, it is no longer available.
+   *  Returns a new data request that was received or null. Once the 
+   *  request is picked up, it is no longer available. This method
+   *  returns immediately and doesn't block.
    *
-   *  @throw IllegalStateException
-   *            if you try to read the request immediatly after is was 
-   *            successfuly read.
+   *  @throws IOException
+   *             if the exception was thrown while waiting for request
+   *             or sending a response
+   *
+   *  @return a new data request or null
    */
-  public I read()
+  public I read() throws IOException
   {
+    if (exception != null) throw exception;
     return request.tryGet();
   }
 
   /**
    *  It writes a response, if there is one and then it waits for a new
-   *  request. Then it end. This method is intended to be run as a parallel
-   *  thread, do not call this method directly.
+   *  request. Then it ends. This method is intended to be run as a parallel
+   *  thread, that is run by <code>write</code> method.
+   *
+   *  <p>If an <code>IOException</code> is catched while waiting for new
+   *  data request or while sending a response, the given input and output
+   *  stream is closed and <code>isActive</code> method starts to return
+   *  <code>false</code> value.
+   *
+   *  @throws IllegalStateException
+   *             if this method is invoked without a response to sent
+   *
+   *  @see #write
    */
   public void run()
   {
+    if (exception != null) return;
     try
     {
-      // send response
-      if (gotRequest && !response.isEmpty())
+      // send response if there is one
+      if (!response.isEmpty())
       {
 	finest("Going to send response");
         outputStream.write(response.get());
 	gotRequest = false;
       }
-      // wait for request
-      request.forcedSet(inputStream.readMessage());
-      finest("Got request");
-      gotRequest = true;
+      if (!gotRequest)
+      {
+        // wait for request
+        request.forcedSet(inputStream.readMessage());
+        finest("Got new data request");
+        gotRequest = true;
+      }
+      else
+      {
+	throw new IllegalStateException(
+	    "Doesn't have a response for a data request to sent!");
+      }
     }
     catch (IOException e)
     {
       exception = e;
       catched(getClass().getName(), "run", e);
+      warning("Catched IOException and closing the connection!");
+      close();
     }
   }
 
+  /**
+   *  Returns true if the connection is active and no exception was
+   *  already catched, false otherwise.
+   */
   public boolean isActive()
   {
     return exception == null;
--- src/control4j/protocols/tcp/Server.java
+++ src/control4j/protocols/tcp/Server.java
@@ -1,7 +1,7 @@
 package control4j.protocols.tcp;
 
 /*
- *  Copyright 2013, 2014 Jiri Lidinsky
+ *  Copyright 2013, 2014, 2015 Jiri Lidinsky
  *
  *  This file is part of control4j.
  *
@@ -22,30 +22,38 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
 import java.io.EOFException;
+
 import static control4j.tools.Logger.*;
 
 /**
  *
- *  Listen on the specified port, and creates a client for each new request.
- *  The server runs as a separate thread and repeatedly tries to recreate
- *  server socket whenever something goes wrong. The thead must be started
- *  from outside.
+ *  Listen at the specified port, and creates a client for each new request
+ *  for connection. The server runs as a separate thread and repeatedly 
+ *  tries to recreate server socket whenever something goes wrong. The 
+ *  thead must be started from outside.
+ *
+ *  @see IClientFactory
  *
  */ 
 public class Server extends Thread
 {
 
+  /** Port to listen at for new connection requests. */
   private int port = 51234;
   
+  /** A factory object that creates new client which will serve each new 
+      request for connection. */
   private IClientFactory clientFactory;
 
   /**
+   *  Initialization.
+   *
    *  @param port
-   *             a port to listen to
+   *             a port to listen at
    *
    *  @param clientFactory
    *             a class that gives an appropriate client which can
-   *             serve the new request for connection
+   *             serve the new connection request
    */
   public Server(int port, IClientFactory clientFactory)
   {
@@ -54,6 +62,17 @@ public class Server extends Thread
     this.clientFactory = clientFactory;
   }
 
+  /**
+   *  Runs an infinite loop in which it listens on the specified port
+   *  and creates a new client each time it receives a new connection
+   *  request. If attempt to create server socket fails, it will try
+   *  to recreate it again and again ... To create new client for
+   *  incoming connection, it calls <code>newClient</code> method
+   *  of the given <code>IClientFactory</code>.
+   *
+   *  <p>This method must be run as a separate thread from outside
+   *  this object.
+   */
   @Override 
   public void run()
   {
@@ -62,8 +81,9 @@ public class Server extends Thread
       try
       {
         ServerSocket serverSocket = new ServerSocket(port);
-        fine("A new ServesSocket successfuly created");
-        while (true)
+        info("A new ServerSocket was successfuly created");
+	boolean error = false;
+        while (!error)
         {
           try
           {
@@ -73,12 +93,17 @@ public class Server extends Thread
           catch (IOException ioe)
           {
             catched(getClass().getName(), "run", ioe);
+	    warning("An exception while waiting for new connection " +
+		"going to recreate server socket ...");
+	    error = true;
+            try { serverSocket.close(); } catch (IOException ioex) { }
           }
         }
       }
       catch (IOException e)
       {
         catched(getClass().getName(), "run", e);
+	warning("An attempt to create server socket failed!");
         long waitTime = Math.round(Math.random() * 4000.0 + 1000.0);
         try { Thread.sleep(waitTime); } catch (InterruptedException ie) {}
       }
