c56560d571cec3cf5022530bd7e9d55f25bc1746
jlidinsky <jlidinsky@gmail.com>
2015-03-20 11:24:54 +0100

    As a follow-up to the changes in the application loading, it was neccessary to changle even the executive module API.

--- src/control4j/Module.java
+++ src/control4j/Module.java
@@ -19,8 +19,7 @@ package control4j;
  */
 
 import java.lang.reflect.Field;
-import control4j.application.ModuleDeclaration;
-import control4j.application.SignalManager;
+
 import control4j.tools.DeclarationReference;
 import static control4j.tools.LogMessages.*;
 
@@ -53,90 +52,12 @@ public abstract class Module
   protected void initialize(IConfigBuffer configuration) 
   { 
     // assign configuration items
-    ConfigurationHelper.assignConfiguration(this, configuration, declarationReference);
-    // assign resources
-    ResourceManager.getInstance().assignResources(this, configuration);
+    ConfigurationHelper.assignConfiguration(
+	this, configuration, declarationReference);
   }
-
-  /**
-   *  Initialize internal structures of the module which are mandatory.
-   *  If you (a module developer) want to influence the module configuration,
-   *  override the {@link #initialize(IConfigBuffer)} method.
-   */
-  protected abstract void initialize(ModuleDeclaration declaration);
 
   public void prepare()
   { }
-
-  /**
-   *  Method that do some preparation work, like collection of input signals
-   *  and than it calls the execution method of the appropriate module 
-   *  implementation. Do not override this method!
-   *
-   *  @param data
-   *             a buffer where to get input signals and where to
-   *             return the output signals
-   */
-  abstract void execute(DataBuffer data);
-
-  /**
-   *  Creates and returns module instance. Returned instance is
-   *  initialized according to the declaration argument.
-   *
-   *  <ol>
-   *    <li> Creates an instance of the appropriate class.
-   *    <li> Sets the information about the declaration reference.
-   *    <li> Calles the {@link #initialize(ModuleDeclaration)} method.
-   *    <li> If module implements a CycleEventListener interface,
-   *         the module is registered to obtain these events.
-   *  </ol>
-   *
-   *  @param declaration
-   *             module declaration
-   *
-   *  @return module instance
-   *
-   *  @throws SyntaxErrorException
-   *             if class doesn't exist
-   *
-   *  @throws ModuleImplementationException
-   *             if implementation of the module is not correct
-   */
-  public static Module getInstance(ModuleDeclaration declaration)
-  {
-    String className = declaration.getClassName();
-    try
-    {
-      // get module class
-      Class<Module> moduleClass = (Class<Module>)(Class.forName(className));
-      // get module instance
-      Module module = moduleClass.newInstance();
-      // set the declaration reference
-      module.declarationReference = declaration.getDeclarationReference();
-      // initialize the module
-      module.initialize(declaration);
-      // register for cycle events listen
-      if (module instanceof ICycleEventListener)
-        Control.registerCycleEventListener((ICycleEventListener)module);
-      return module;
-    }
-    catch (ClassNotFoundException e)
-    {
-      throw new SyntaxErrorException("Class was not found");
-    }
-    // if this class represents an abstrac class, interface, an array ...
-    catch (InstantiationException e)
-    {
-      String message = getMessage("module01", className, e.getMessage());
-      throw new ModuleImplementationException(message);
-    }
-    // constructor is not accessible
-    catch (IllegalAccessException e)
-    {
-      String message = getMessage("module02", className, e.getMessage());
-      throw new ModuleImplementationException(message);
-    }
-  }
 
   /**
    *  Returns a reference to the config file
@@ -156,32 +77,6 @@ public abstract class Module
   }
 
   /**
-   *  Assign a unit that is specified in the signal declaration to all of 
-   *  the given signals which doesn't have already the unit attached.
-   *
-   *  @param map
-   *             a mapping from signals array into the signal declaration
-   *             indexes
-   *
-   *  @param signals
-   *             signals that will be updated
-   */
-  protected void assignUnits(int[] map, Signal[] signals)
-  {
-    SignalManager signalManager = SignalManager.getInstance();
-    for (int i=0; i<map.length; i++)
-    {
-      if (map[i] >= 0 
-          && (signals[i].getUnit() == null 
-          || signals[i].getUnit().length() == 0))
-      {
-	String unit = signalManager.get(map[i]).getUnit();
-	signals[i].setUnit(unit);
-      }
-    }
-  }
-
-  /**
    *  Prints information about the module into the given writer.
    *  It serves mainly for debug purposes. This method writes only
    *  the declaration reference, if you thing that it would be useful
@@ -197,32 +92,6 @@ public abstract class Module
     writer.println("== MODULE ==");
     writer.println("Class: " + getClass().getName());
     writer.println(declarationReference.toString());
-  }
-
-  /**
-   *  Writes information about input or output of the module into
-   *  the given writer. It serves mainly for debug purposes.
-   *
-   *  @param map
-   *             mapping from input or output indexes of this module
-   *             into the data buffer indexes
-   *
-   *  @param writer
-   *             a writer on which the information will be printed
-   */
-  protected void dumpIO(int[] map, java.io.PrintWriter writer)
-  {
-    writer.print("[");
-    if (map.length > 0)
-    {
-      writer.print(map[0]);
-      for (int i=1; i<map.length; i++)
-      {
-	writer.print(", ");
-	writer.print(map[i]);
-      }
-    }
-    writer.print("]");
   }
 
 }
--- src/control4j/OutputModule.java
+++ src/control4j/OutputModule.java
@@ -1,7 +1,7 @@
 package control4j;
 
 /*
- *  Copyright 2013, 2014 Jiri Lidinsky
+ *  Copyright 2013, 2014, 2015 Jiri Lidinsky
  *
  *  This file is part of control4j.
  *
@@ -19,10 +19,8 @@ package control4j;
  */
 
 import java.util.NoSuchElementException;
+
 import control4j.application.Output;
-import control4j.application.ModuleDeclaration;
-import control4j.application.SignalDeclaration;
-import control4j.application.SignalManager;
 
 /**
  *  Represents a module which provides only output for another processing.
@@ -31,106 +29,25 @@ public abstract class OutputModule extends Module
 {
 
   /**
-   *  Array that maps module outputs into the array of signal values
-   *  and signal declarations. Not all of the outputs must be assigned.
-   *  unassigned outputs have negative values.
-   */
-  private int[] outputMap;
-  
-  /**
-   *  Initialize the internal structures of the module based on the
-   *  declaration. Namely, it createas and assignes the input and
-   *  output map arrays, calls {@link #initialize(IConfigBuffer)}
-   *  method to provide custom configuration and calls 
-   *  {@link #setOutputConfiguration} method for each output to
-   *  perform custom output configuration.
-   *
-   *  <p>This method is called only once, during the phase of 
-   *  application building by the {@link Module#getInstance} method.
-   *  
-   *  @param declaration
-   *             module declaration. May not have null value
-   *
-   *  @see #initialize(IConfigBuffer)
-   *  @see #setOutputConfiguration
-   *  @see Module#getInstance
-   */
-  protected void initialize(ModuleDeclaration declaration)
-  {
-    // output map initialization
-    initializeOutputMap(declaration);
-    // calls custom configuration
-    IConfigBuffer configuration = declaration.getConfiguration();
-    initialize(configuration);
-    // calls output configuration
-    for (int i=0; i<declaration.getOutputsSize(); i++)
-    {
-      Output output = declaration.getOutput(i);
-      if (output != null)
-      {
-        configuration = output.getConfiguration();
-        setOutputConfiguration(i, configuration);
-      }
-    }
-  }
-  
-  /**
    *  Method that provides output of the module for the further prcessing.
    *  This is the only method that must be overwritten by the module
    *  developer. This method will be called repeatedly by the execute,
    *  method during the processing phase.
    *
-   *  @return the module output. The size of the returned array must
-   *             be at least the size returned by the 
-   *             {@link #getNumberOfAssignedOutputs} method. If it is
-   *             larger, extra elements will be ignored. The 
-   *             mandatory elements may not contain null values.
+   *  @param output
+   *             an array which serves for output transfer.
+   *
+   *  @param outputLength
+   *             how may elements may be used for this method
    *
    *  @throws RuntimeModuleException
    *             if something went wrong and the whole control loop
    *             should not be finished. But sometimes it is sufficient
    *             just return invalid signals.
    *
-   *  @see #execute
-   *  @see #getNumberOfAssignedOutputs
    */
-  protected abstract Signal[] get();
-
-  /**
-   *  Calls {@link #get} method and then places the returned signals
-   *  into the data buffer which is given as parameter. This method
-   *  is called repeatedly by the instance of ControlLoop object during
-   *  the processing phase.
-   *
-   *  @param data
-   *             a data buffer to store output of the module
-   *
-   *  @see ControlLoop
-   */
-  @Override
-  final void execute(DataBuffer data)
-  {
-    // call the exec function
-    Signal[] output = get();
-    // assign units
-    assignUnits(outputMap, output);
-    // store the output into the data buffer
-    data.put(output, outputMap);
-  }
-
-  /**
-   *  Returns number of outputs which will be used for the further
-   *  processing. This number may be smaller than the amount of 
-   *  outputs that are provided by this module, but may not be 
-   *  greater.
-   *
-   *  @return the number of outputs which are expecting that this
-   *             will provide.
-   */
-  public int getNumberOfAssignedOutputs()
-  {
-    return outputMap.length;
-  }
+  protected abstract void get(Signal[] output, int outputLength)
+      throws RuntimeModuleException;
 
   /**
    *  This method is used for custom output configuration. This method
@@ -139,8 +56,7 @@ public abstract class OutputModule extends Module
    *  method.
    *
    *  <p>This method is called only once during the phase of application
-   *  building by the method {@link #initialize(IConfigBuffer)} for
-   *  each output.
+   *  building by the method for each output.
    *
    *  <p>If parameters contain configuration that is not supported by
    *  this module or output, throws SyntaxErrorException.
@@ -156,72 +72,10 @@ public abstract class OutputModule extends Module
   public void setOutputConfiguration(int index, IConfigBuffer configuration)
   { }
 
-  /**
-   *  Returns a signal declaration object for output with
-   *  given index. It is useful if module need informations such
-   *  is the signal name or unit.
-   *
-   *  @param index
-   *            module output index
-   *
-   *  @return signal declaration or null if output with given index
-   *            is not used
-   *
-   *  @throws IndexOutOfBoundsException
-   *            if index parameter is less then zero or greater 
-   *            then number of assigned outputs
-   */
-  protected SignalDeclaration getOutputSignalDeclaration(int index)
-  {
-    int handler = outputMap[index];
-    if (handler >= 0)
-      return SignalManager.getInstance().get(handler);
-    else
-      return null;
-  }
-
-  public int[] getOutputMap()
-  {
-    return outputMap;
-  }
-
-  private void initializeOutputMap(ModuleDeclaration declaration)
-  {
-    // allocate the input indexes array
-    int size = declaration.getOutputsSize();
-    outputMap = new int[size];
-    // fill in the array of output indexes
-    SignalManager signalManager = SignalManager.getInstance();
-    for (int i=0; i<size; i++)
-    {
-      Output output = declaration.getOutput(i);
-      if (output == null)
-        outputMap[i] = -1;
-      else
-      {
-	try
-	{
-          outputMap[i] 
-	    = signalManager.getHandler(output.getScope(), output.getSignal());
-	}
-	catch (NoSuchElementException e)
-	{
-	  // !!! vyjimka, nedefinovany signal
-	  ErrorManager.getInstance()
-	    .reportUndeclaredSignal(output.getDeclarationReferenceText());
-	  outputMap[i] = -1;
-	}
-      }
-    }
-  }
-
   @Override
   public void dump(java.io.PrintWriter writer)
   {
     super.dump(writer);
-    writer.print("Output map: ");
-    dumpIO(outputMap, writer);
-    writer.println();
   }
 
 }
