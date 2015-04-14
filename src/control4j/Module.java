2be244ab66dbfbc74e45b7613e8fc2118aea929f
jlidinsky <jlidinsky@gmail.com>
2015-04-14 11:01:17 +0200

    Methods for module input size checks added.

--- src/control4j/Module.java
+++ src/control4j/Module.java
@@ -31,6 +31,54 @@ import static control4j.tools.LogMessages.*;
 
 /**
  *  Module is one of the main building blocks of the application.
+ *  A module gets input and provides output.
+ *
+ *  <p>Number of input signals (terminals) and input terminal
+ *  meaning depends entirely on the module implementation. Each
+ *  module gets all of its input in one array, during the runtime,
+ *  so each input terminal has a zero-based index to distinguish
+ *  it. Because the number of input terminals doesn't have to be
+ *  known during the module creation, and moreover there may be
+ *  modules for which the order of input signals is meaningless,
+ *  the input terminals must be organized inside the input array
+ *  as follows:
+ *  <ol>
+ *    <li>Indexes from zero to some <em>n</em> are dedicated to
+ *        terminals which are mandatory. Elements inside the
+ *        input array may not contain <code>null</code> value.
+ *        In other words, these input terminals must be connected.
+ *        Concrete module implementation must eiter override
+ *        a method <code>getMandatoryInputSize</code> to return
+ *        the proper number of mandatory input terminals or the
+ *        <code>AMinInput</code> annotation my be used to achive
+ *        the correct functionality.
+ *    <li>Indexes from <em>n</em> to some <em>m</em> are used
+ *        by terminals which are optional. Moreover, the <em>m</em>
+ *        may not be known by the time of module creation, it
+ *        means, that in such a case, it depends entirely on the
+ *        application definition, how many input signals will be
+ *        connected. Nevertheless the terminals which span into
+ *        this category may left disconnected. In other words,
+ *        the corresponding element in the input array may contain
+ *        <code>null</code> value. Module implementation must
+ *        either override the method <code>getIndexedInputSize</code>
+ *        to return the proper number of totally supported indexed
+ *        input terminals, or it may use <code>AMaxInput</code>
+ *        for the same purpose.
+ *    <li>The third category of input is index-less input. If
+ *        this kind of input is supported by the module, than the
+ *        method <code>isVariableInputSupported</code> must return
+ *        <code>true</code>. Same functionality may be achieved
+ *        thanks to the <code>AVariableInput</code> annotation.
+ *        The only condition that must be satisfied is that number
+ *        of input terminals that belongs to the previous two
+ *        categoris is bounded. In such a case, index-less signals
+ *        will ocupy input array indexes starting from <em>m</em>.
+ *  </ol>
+ *
+ *  @see AVariableInput
+ *  @see AMinInput
+ *  @see AMaxInput
  */
 public abstract class Module
 {
@@ -62,10 +110,110 @@ public abstract class Module
   }
 
   /**
+   *  Checks that it is possible to connect a signal into
+   *  the input with given index.
    *
+   *  <p>Implicite implementation uses annotations to determine
+   *  which index may be used to connect a signal.
+   *
+   *  @param index
+   *
+   *  @param inputSize
+   *
+   *  @throws
+   *
+   *  @see AMaxInput
+   *  @see AMinInput
+   *  @see AVariableInput
    */
   protected void checkConnectedInput(int index, int inputSize)
   {
+    if (index >= inputSize)
+      throw new IllegalArgumentException();  // TODO:
+  }
+
+  protected void checkDisconnectedInput(int index, int inputSize)
+  {
+    if (index < getMandatoryInputSize())
+      throw new IllegalArgumentException(); // TODO:
+  }
+
+  /**
+   *  Returns number of input terminals which belongs to the
+   *  category of mandatory (not optional) terminals. These
+   *  are terminals that must be connected to guarantee correct
+   *  module functionality. This number is entirely the property
+   *  of the module implementation.
+   *
+   *  <p>This implementation returns a value that is specified
+   *  by a <code>AMinInput</code> annotation. If not declared
+   *  it returns zero.
+   *
+   *  @return number of mandatory input terminals required by
+   *             the module
+   *
+   *  @see AMinInput
+   */
+  public int getMandatoryInputSize()
+  {
+    AMinInput minInputAnno = getClass().getAnnotation(AMinInput.class);
+    if (minInputAnno == null)
+      return 0;
+    else
+      return minInputAnno.value(); // TODO: check that the value is positive
+  }
+
+  /**
+   *  Returns number of indexed input terminals which are supported
+   *  by this module.
+   *
+   *  <p>This implementation returns a value that is specified by
+   *  the <code>AMaxInput</code> annotation. If not declared, it
+   *  returns the number of mandatory input terminals.
+   *
+   *  @return total number of indexed input terminals supported
+   *             by this module
+   *
+   *  @see AMaxInput
+   */
+  public int getIndexedInputSize()
+  {
+    AMaxInput maxInputAnno = getClass().getAnnotation(AMaxInput.class);
+    if (maxInputAnno == null)
+      return getMandatoryInputSize();
+    else
+      return maxInputAnno.value(); // TODO: check that the value is positive
+  }
+
+  /**
+   *  Returns true iff this module supports a variable input.
+   *  It means input where signal order is meaningless.
+   */
+  public boolean isVariableInputSupported()
+  {
+    return getClass().getAnnotation(AVariableInput.class) != null;
+  }
+
+  /**
+   *  Returns index of the first possible variable input.
+   */
+  public int getVariableInputFirstIndex()
+  {
+    if (!isVariableInputSupported())
+      throw new UnsupportedOperationException();
+    return getIndexedInputSize();
+  }
+
+  /**
+   *
+   */
+  public int getInputSize(int lastDeclaredIndex)
+  {
+    int mandatorySize = getMandatoryInputSize();
+    if (lastDeclaredIndex < mandatorySize - 1)
+      return mandatorySize;
+    else
+      return lastDeclaredIndex + 1;
   }
 
   public void prepare()
