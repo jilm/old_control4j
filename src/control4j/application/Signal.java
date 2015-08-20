b89d57d8c599dc1e9539331899e7e60a286f4177
jlidinsky <jlidinsky@gmail.com>
2015-08-20 12:45:31 +0200

    Add unit property into the signal definition class.

--- src/control4j/application/Signal.java
+++ src/control4j/application/Signal.java
@@ -18,56 +18,62 @@ package control4j.application;
  *  along with control4j.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import java.util.HashMap;
-import java.util.Set;
-
 import control4j.tools.DeclarationReference;
 
 import cz.lidinsky.tools.ToStringBuilder;
+import cz.lidinsky.tools.Validate;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
 
 /**
  *
- *  Definition of the signal.
+ *  Keeps signal definition.
+ *
+ *  <p>This object provides following informations for use by the modules:
+ *
+ *  <ul>
+ *
+ *    <li> <em>Label</em> is the text identification which is intended mainly
+ *    for MMI clients.
+ *
+ *  </ul>
  *
  */
-public class Signal extends Configurable
-{
+public class Signal extends Configurable {
 
   private String name;
 
   /**
-   *  Crate a new empty signal definition object.
+   *  Crate a new empty signal definition object. The parameter name must be
+   *  the name specified inside the application document.
    */
   public Signal(String name) {
-    this.name = name;
+    this.name = Validate.notBlank(name);
   }
 
-  /*
-   *
-   *     Value for time T-1
-   *
-   */
+  //----------------------------------------------------- Default Signal Value.
 
   private boolean isValueT_1 = false;
   private boolean isValueT_1Valid = false;
   private String valueT_1;
 
-  public void setValueT_1Invalid()
-  {
+  public void setValueT_1Invalid() {
     isValueT_1 = true;
     isValueT_1Valid = false;
     valueT_1 = null;
   }
 
-  public void setValueT_1(String value)
-  {
+  public void setValueT_1(String value) {
     isValueT_1 = true;
     isValueT_1Valid = true;
     valueT_1 = value;
   }
 
-  public boolean isValueT_1Specified()
-  {
+  public boolean isValueT_1Specified() {
     return isValueT_1;
   }
 
@@ -79,39 +85,62 @@ public class Signal extends Configurable
     return Double.parseDouble(valueT_1);
   }
 
-  /*
-   *
-   *    Tags
-   *
-   */
+  //--------------------------------------------------------------------- Tags.
 
   private HashMap<String, Tag> tags = new HashMap<String, Tag>();
 
-  public void putTag(String name, Tag tag)
-  {
+  public void putTag(String name, Tag tag) {
     tags.put(name, tag);
   }
 
-  public Set<String> getTagNames()
-  {
+  public Set<String> getTagNames() {
     return tags.keySet();
   }
 
-  public Tag getTag(String name)
-  {
+  public Tag getTag(String name) {
     return tags.get(name);
   }
 
+  //-------------------------------------------------------------------- Label.
+
   private String label;
 
+  /**
+   *  Returns the label, which is short textual identification of the signal
+   *  which is dedicated mainly for use by the MMI clients and programs. If the
+   *  label has not been set yet, or if it has been set to blank value, the
+   *  name is returned instead.
+   */
   public String getLabel() {
-    // TODO:
-    return label == null ? name : label;
+    return StringUtils.isBlank(label) ? name : label;
   }
 
+  /**
+   *  Sets the label property of the signal.
+   */
   public void setLabel(String label) {
     this.label = label;
   }
+
+  //--------------------------------------------------------------------- Unit.
+
+  private String unit;
+
+  /**
+   *  Returns unit of the signal.
+   */
+  public String getUnit() {
+    return unit == null ? "" : unit;
+  }
+
+  /**
+   *  Sets the unit of the signal.
+   */
+  public void setUnit(String unit) {
+    this.unit = unit;
+  }
+
+  //-------------------------------------------------------------------- Other.
 
   @Override
   public void toString(ToStringBuilder builder)
